8f2f9b5947aeaf041547dca613cfccfaa38087bb
Victor Mwadime <victormwadime@gmail.com>
2025-08-19 21:01:57 +0300

    create Topbar component

---
+++ client/src/components/Topbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Container, Button } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+
+export default function Topbar() {
+  return (
+    <AppBar position="static" color="primary" elevation={0}>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters sx={{ gap: 2 }}>
+          <Typography
+            variant="h6"
+            component={RouterLink}
+            to="/"
+            sx={{ color: "inherit", textDecoration: "none", flexGrow: 1 }}
+          >
+            Books Inventory
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
