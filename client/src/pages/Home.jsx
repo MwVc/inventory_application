dfe1f544132fdfb8d52ef7b21fc2cc37ee08b649
Victor Mwadime <victormwadime@gmail.com>
2025-08-20 12:30:48 +0300

    import components and router

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,29 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { useBooks } from "../context/BooksContext";
+import BookTable from "../components/BookTable";
+import Button from "@mui/material/Button";
+import { Link as RouterLink } from "react-router-dom";
+
+export default function Home() {
+  const { list, remove } = useBooks();
+  const books = list();
+
+  return (
+    <Box>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        mb={2}
+      >
+        <Typography variant="h5">Books</Typography>
+        <Button component={RouterLink} to="/add" variant="contained">
+          Add Book
+        </Button>
+      </Box>
+      <BookTable books={books} onDelete={remove} />
+    </Box>
+  );
+}
