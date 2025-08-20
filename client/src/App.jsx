b8067f1ea27e7f4e596b46155956a20c8c950829
Victor Mwadime <victormwadime@gmail.com>
2025-08-20 12:31:41 +0300

    import components pages and router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Container, Box } from "@mui/material";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 import Topbar from "./components/Topbar";
+import Home from "./pages/Home";
+import AddBook from "./pages/AddBook";
+import EditBook from "./pages/EditBook";
+import BookDetails from "./pages/BookDetails";
+import NotFound from "./pages/NotFound";
+import { BooksProvider } from "./context/BooksContext";
 
 export default function App() {
   return (
-    <>
+    <BooksProvider>
       <Topbar />
       <Box component="main" sx={{ py: 4 }}>
         <Container maxWidth="lg">
-          <Routes></Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add" element={<AddBook />} />
+            <Route path="/edit/:id" element={<EditBook />} />
+            <Route path="/books/:id" element={<BookDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </Container>
       </Box>
-    </>
+    </BooksProvider>
   );
 }
