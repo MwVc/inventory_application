57868cafbdb1d5c48e9f6d1d1647a57776bee952
Victor Mwadime <victormwadime@gmail.com>
2025-08-25 16:33:23 +0300

    import queries and router

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 require("dotenv").config();
+const { getBooks } = require("./getBooks");
+const { getData } = require("./db/queries");
+const indexRouter = require("./routes/indexRouter.js");
 
 const PORT = process.env.PORT || 5050;
 
 const app = express();
+
+app.get("/", indexRouter);
 
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}`);
