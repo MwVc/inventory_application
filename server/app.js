e317d45e27a0d06c1ecafef9aeee75cef65cf638
Victor Mwadime <victormwadime@gmail.com>
2025-09-13 10:56:32 +0300

    remove unsused imports from app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 require("dotenv").config();
-const { getBooks } = require("./getBooks");
-const { getData } = require("./db/queries");
-const indexRouter = require("./routes/indexRouter.js");
-
 const PORT = process.env.PORT || 5050;
+
+// import routes
+const indexRouter = require("./routes/indexRouter");
 
 const app = express();
 
