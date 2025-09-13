a9ef366324f7c8753cc66306270cb6ffbaa3dda4
Victor Mwadime <victormwadime@gmail.com>
2025-09-13 17:29:17 +0300

    refactor remove error handlig and rename it to getBooks

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,14 +1,10 @@
 const pool = require("./pool");
 
-async function getData() {
-  try {
-    const { rows } = await pool.query("SELECT * FROM usernames");
-    return rows;
-  } catch (error) {
-    throw new Error(`Database error: ${error.message}`);
-  }
+async function getBooks() {
+  const { rows } = await pool.query("SELECT * FROM books");
+  return rows;
 }
 
 module.exports = {
-  getData,
+  getBooks,
 };
