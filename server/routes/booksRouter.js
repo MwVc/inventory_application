9d67c961a9d9a1742c2155238a720f20cf36cd08
Victor Mwadime <victormwadime@gmail.com>
2025-09-15 22:13:04 +0300

    import createBook controller

--- server/routes/booksRouter.js
+++ server/routes/booksRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { getAllBooks, getBookById } = require("../controllers/booksController");
+const {
+  getAllBooks,
+  getBookById,
+  createBook,
+} = require("../controllers/booksController");
 const router = express.Router();
 
 router.get("/books", getAllBooks);
 router.get("/books/:id", getBookById);
-// router.put("/books/create", createBook);
+router.post("/books/create", createBook);
 
 module.exports = router;
