13ea4e7ec175c5f8cb2ce972c288d8af8cf6fdce
Victor Mwadime <victormwadime@gmail.com>
2025-09-15 11:16:20 +0300

    create getBookById controller that returns json; standardize db function naming for consistency; update module exports to include getBookById controller

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -1,12 +1,24 @@
-const { getBooks } = require("../db/queries");
+const { dbGetBooks, dbGetBookById } = require("../db/queries");
 
 const getAllBooks = async (req, res, next) => {
   try {
-    const books = await getBooks();
+    const books = await dbGetBooks();
     res.status(200).json(books);
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { getAllBooks };
+const getBookById = async (req, res, next) => {
+  const bookId = req.params.id;
+
+  try {
+    const book = await dbGetBookById(bookId);
+    res.status(200).json({ book: book });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+module.exports = { getAllBooks, getBookById };
