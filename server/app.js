5ae33140dc4f0a3b3e609c0f9998295dcc4f7eda
Victor Mwadime <victormwadime@gmail.com>
2025-09-15 22:10:50 +0300

    import cors to allow api calls; use express.json middleware for formdata submission

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const PORT = process.env.PORT || 5050;
+
+const app = express();
 
 // import routes
 const booksRouter = require("./routes/booksRouter");
@@ -8,7 +11,8 @@ const booksRouter = require("./routes/booksRouter");
 // import error handler
 const errorHandler = require("./middleware/errorMiddleware");
 
-const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.use("/api", booksRouter);
 app.use(errorHandler);
