99c003320ddf82981d5afb3b362e19fd2f1c7011
Victor Mwadime <victormwadime@gmail.com>
2025-09-15 11:22:12 +0300

    rename getBooks to dbGetBooks for clarity; create dbGetBookById to fetch book with its genre; implemented error handling when book is not found; update module export

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,10 +1,23 @@
 const pool = require("./pool");
 
-async function getBooks() {
+async function dbGetBooks() {
   const { rows } = await pool.query("SELECT * FROM books");
   return rows;
 }
 
+async function dbGetBookById(bookId) {
+  const { rows } = await pool.query(
+    "SELECT books.*, genres.name AS genre_name FROM books INNER JOIN genres ON books.genre_id = genres.id WHERE books.id = ($1)",
+    [bookId]
+  );
+  if (rows.length === 0) {
+    throw new Error(`Book not found`);
+  }
+
+  return rows;
+}
+
 module.exports = {
-  getBooks,
+  dbGetBooks,
+  dbGetBookById,
 };
