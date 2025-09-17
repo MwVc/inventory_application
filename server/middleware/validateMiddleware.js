20618ec3ab4d74fa2b2c31dbcbc9c5accf7fb639
Victor Mwadime <victormwadime@gmail.com>
2025-09-17 11:05:05 +0300

    expand book validation rules; improve validation with type and length checks

--- server/middleware/validateMiddleware.js
+++ server/middleware/validateMiddleware.js
@@ -1,8 +1,30 @@
 const { body, validationResult } = require("express-validator");
 
 const validateBook = [
-  body("title").notEmpty().withMessage("Title cannot be empty"),
-  body("stock").notEmpty().withMessage("Stock cannot be empty"),
+  body("title")
+    .notEmpty()
+    .withMessage("Title is required")
+    .isString()
+    .withMessage("Title must be a string")
+    .isLength({ min: 2 })
+    .withMessage("Title must be at keast 2 characters long"),
+  body("stock")
+    .notEmpty()
+    .withMessage("Stock is required")
+    .isInt({ min: 0 })
+    .withMessage("Stock must be 0 or higher"),
+  body("genre_id")
+    .notEmpty()
+    .withMessage("Genre is required")
+    .isInt({ min: 1 })
+    .withMessage("Genre must be a positive number"),
+  body("author")
+    .notEmpty()
+    .withMessage("Author is required")
+    .isString()
+    .withMessage("Author must be a string")
+    .isLength({ min: 2 })
+    .withMessage("Author name must be at least  characters long"),
 ];
 
 module.exports = { validateBook };
