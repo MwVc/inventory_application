4cd792d5606de22087843ce92ca3c93b41f1fbe2
Victor Mwadime <victormwadime@gmail.com>
2025-09-18 21:49:41 +0300

    remove console.error logging

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,5 +1,5 @@
 function errorHandler(err, req, res, next) {
-  console.error("Error stack:", err); // logs stack trace to the console
+  // console.error("Error stack:", err); // logs stack trace to the console
   res
     .status(err.statusCode || 500)
     .json({ error: err.message || "Internal Server Error" });
