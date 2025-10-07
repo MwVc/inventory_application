95b129f31a0262a7abaca9ea714bce2744dc2dfd
Victor Mwadime <victormwadime@gmail.com>
2025-10-07 08:43:37 +0300

    add patch route for updating books

--- server/routes/booksRouter.js
+++ server/routes/booksRouter.js
@@ -4,6 +4,7 @@ const {
   getBookById,
   createBook,
   deleteBook,
+  updateBook,
 } = require("../controllers/booksController");
 const { validateBook } = require("../middleware/validateMiddleware");
 const router = express.Router();
@@ -11,6 +12,7 @@ const router = express.Router();
 router.get("/books", getAllBooks);
 router.get("/books/:id", getBookById);
 router.post("/books/create", [validateBook, createBook]);
+router.patch("books/:book_id", [validateBook, updateBook]);
 router.delete("/books/:book_id", deleteBook);
 
 module.exports = router;
