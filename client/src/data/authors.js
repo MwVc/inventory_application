4c55a9f6c7d4bc702a9a05a514b55552a8329987
Victor Mwadime <victormwadime@gmail.com>
2025-10-07 08:19:57 +0300

    restart project from scratch

--- client/src/data/authors.js
+++
@@ -1,5 +0,0 @@
-export const authors = [
-  { id: 1, name: "Chinua Achebe", books: 5 },
-  { id: 2, name: "Ngũgĩ wa Thiong’o", books: 7 },
-  { id: 3, name: "Margaret Ogola", books: 3 },
-];
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import "./styles.css";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const theme = createTheme({
-  palette: { mode: "light" },
-});
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
