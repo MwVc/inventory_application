e043acb85a9cb1f1b08d56acec875ff334f6d9ae
Victor Mwadime <victormwadime@gmail.com>
2025-10-07 11:29:01 +0300

    Import Dashboard component and render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
-  return <div className="text-3xl font-bold underline">App</div>;
+  return <Dashboard />;
 };
 
 export default App;
