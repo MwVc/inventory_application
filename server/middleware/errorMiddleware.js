d3963424fcdf47ae131fb2cd3ae552db5e78ddb3
Victor Mwadime <victormwadime@gmail.com>
2025-10-16 10:01:25 +0300

    fix: update error structure to include success status in errorMiddleware

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -2,7 +2,7 @@ function errorHandler(err, req, res, next) {
   // console.error("Error stack:", err); // logs stack trace to the console
   res
     .status(err.statusCode || 500)
-    .json({ error: err.message || "Internal Server Error" });
+    .json({ success: false, error: err.message || "Internal Server Error" });
 }
 
 module.exports = errorHandler;
