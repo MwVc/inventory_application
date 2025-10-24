d80d97a17ea311d0a32c3af25998c1b6619efe56
Victor Mwadime <victormwadime@gmail.com>
2025-10-24 15:10:43 +0300

    feat: update dbGetGenres to include book count in genre retrieval

--- server/db/genresQueries.js
+++ server/db/genresQueries.js
@@ -1,7 +1,9 @@
 const pool = require("./pool");
 
 const dbGetGenres = async () => {
-  const { rows } = await pool.query("SELECT * FROM genres;");
+  const { rows } = await pool.query(
+    "SELECT g.id, g.name, COUNT(b.id) AS books FROM genres g LEFT JOIN books b ON g.id = b.genre_id GROUP BY g.id, g.name;"
+  );
   if (rows.length === 0) {
     const error = new Error("Books not found");
     error.statusCode = 404;
