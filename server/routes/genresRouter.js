dd51ea80391de968d47dc79aaa630c058de8e1c5
Victor Mwadime <victormwadime@gmail.com>
2025-10-26 23:53:21 +0300

    feat: add delete route for genre in genresRouter

--- server/routes/genresRouter.js
+++ server/routes/genresRouter.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 // importing controllers
-const { getAllGenres } = require("../controllers/genresController");
+const {
+  getAllGenres,
+  deleteGenre,
+} = require("../controllers/genresController");
 
 router.get("/", getAllGenres);
+router.delete("/:id", deleteGenre);
 
 module.exports = router;
