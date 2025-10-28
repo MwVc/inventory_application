9de322e3aff66ebdda393033efa02b166de60813
Victor Mwadime <victormwadime@gmail.com>
2025-10-28 13:33:32 +0300

    feat: add password middleware to protect genre deletion route

--- server/routes/genresRouter.js
+++ server/routes/genresRouter.js
@@ -8,9 +8,10 @@ const {
   createGenre,
 } = require("../controllers/genresController");
 const { validateGenre } = require("../middleware/validateMiddleware");
+const passwordMiddleware = require("../middleware/passwordMiddleware");
 
 router.get("/", getAllGenres);
 router.post("/create", [validateGenre, createGenre]);
-router.delete("/:id", [deleteGenre]);
+router.delete("/:id", [passwordMiddleware, deleteGenre]);
 
 module.exports = router;
