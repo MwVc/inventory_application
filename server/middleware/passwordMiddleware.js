1673564dcaa72ad8b5f837ac36f44b0da2d0d6ec
Victor Mwadime <victormwadime@gmail.com>
2025-10-28 13:32:17 +0300

    refactor: remove unnecessary console log

--- server/middleware/passwordMiddleware.js
+++ server/middleware/passwordMiddleware.js
@@ -1,5 +1,4 @@
 function passwordMiddleware(req, res, next) {
-  console.log(req.body);
   const password = "plantcutter";
   if (!req.body.password || req.body.password !== password) {
     res.status(401).json({ success: false, message: "Wrong password" });
