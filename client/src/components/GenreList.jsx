066fc08709110f2cb05a0f3496aa7a0741665ef8
Victor Mwadime <victormwadime@gmail.com>
2025-10-28 09:12:38 +0300

    feat: implement handleGenreFormSubmit to add genre to db and refetch genres on success

--- client/src/components/GenreList.jsx
+++ client/src/components/GenreList.jsx
@@ -1,34 +1,80 @@
-const GenreList = ({ genres }) => {
+import { useState } from "react";
+import { createGenre } from "../api/genresApi";
+import toast from "react-hot-toast";
+
+const GenreList = ({ genres, getGenres }) => {
+  const [genre, setGenre] = useState("");
+
+  // set input value to state - controlled form
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setGenre(value);
+  };
+
+  const handleGenreFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await createGenre(genre);
+
+      setGenre("");
+      getGenres();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <div className="container">
-      <h2 className="text-xl font-semibold mb-2">Genres</h2>
-      <table className="w-full border-collapse">
-        <thead>
-          <tr className="bg-gray-200 text-left">
-            <th className="p-2 border">Genre</th>
-            <th className="p-2 border text-center">No. of Books</th>
-            <th className="p-2 border text-center">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {genres.map((genre) => {
-            return (
-              <tr className="hover:bg-gray-50" key={genre.id}>
-                <td className="p-2 border">{genre.name}</td>
-                <td className="p-2 border text-center">{genre.books}</td>
-                <td className="p-2 border text-center space-x-2">
-                  <button
-                    className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
-                    onClick={() => console.log("Deleting genre")}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div className="container flex flex-col gap-y-9 h-full">
+      <div className="container">
+        <h2 className="text-xl font-semibold mb-2">Genres</h2>
+        <table className="w-full border-collapse">
+          <thead>
+            <tr className="bg-gray-200 text-left">
+              <th className="p-2 border">Genre</th>
+              <th className="p-2 border text-center">No. of Books</th>
+              <th className="p-2 border text-center">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {genres.map((genre) => {
+              return (
+                <tr className="hover:bg-gray-50" key={genre.id}>
+                  <td className="p-2 border">{genre.name}</td>
+                  <td className="p-2 border text-center">{genre.books}</td>
+                  <td className="p-2 border text-center space-x-2">
+                    <button
+                      className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+                      onClick={() => console.log("Deleting genre")}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className="container">
+        <form onSubmit={handleGenreFormSubmit} className="space-y-4">
+          <h2 className="text-xl font-semibold mb-2">Add A New Genre</h2>
+          <input
+            type="text"
+            name="genre"
+            placeholder="Add a new genre"
+            value={genre}
+            onChange={handleChange}
+            className="w-full border rounded-lg p-2"
+            required
+          />
+          <button
+            type="submit"
+            className="bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
+          >
+            Add Genre
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
