97b6d45d1157f1790d34dca66230a07e3bddf157
Victor Mwadime <victormwadime@gmail.com>
2025-10-28 09:19:57 +0300

    feat: add dbCreateGenre function with error handling

--- server/db/genresQueries.js
+++ server/db/genresQueries.js
@@ -13,6 +13,22 @@ const dbGetGenres = async () => {
   return rows;
 };
 
+const dbCreateGenre = async (name) => {
+  console.log(name);
+  const { rowCount } = await pool.query(
+    "INSERT INTO genres (name) VALUES ($1);",
+    [name]
+  );
+
+  if (rowCount === 0) {
+    const error = new Error("Failed to add genre");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  return;
+};
+
 const dbDeleteGenre = async (id) => {
   const { rowCount } = await pool.query("DELETE * FROM genres WHERE id = $1;", [
     id,
@@ -20,4 +36,4 @@ const dbDeleteGenre = async (id) => {
   console.log(rowCount);
 };
 
-module.exports = { dbGetGenres, dbDeleteGenre };
+module.exports = { dbGetGenres, dbDeleteGenre, dbCreateGenre };
