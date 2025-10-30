0a67803c5690966e777d1f14745ed0f3cc9ef7b8
Victor Mwadime <victormwadime@gmail.com>
2025-10-30 09:51:28 +0300

    refactor: update dotenc config to load from parent directory and add connection string log

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,7 +1,10 @@
-require("dotenv").config();
+const path = require("path");
+
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const { Pool } = require("pg");
 
 const connectionString = process.env.DB_CONNECTION_STRING;
+console.log(connectionString);
 
 module.exports = new Pool({
   connectionString: connectionString,
